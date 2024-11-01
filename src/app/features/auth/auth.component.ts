591968d52fb910d40fad81fdf7707928c1ef51e5
GAMELEIRA <gameleira270499@gmail.com>
2024-11-01 10:57:49 -0300

    feat(feature/login): Adiciona componentes visuais para a tela de login

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,10 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PoButtonModule, PoContainerModule, PoFieldModule } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    PoFieldModule,
+    PoButtonModule,
+    PoContainerModule
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
